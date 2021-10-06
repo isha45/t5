1a86ecd6efd849db7e2393a9ba9b9baed9ace06f
isha45 <92019985+isha45@users.noreply.github.com>
2021-10-06 14:15:13 +0530

    Add files via upload

---
+++ compdna.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fun(string s){
+    int n =s.length();
+    int ans = 0;
+    vector<int>cnt(26,0);
+    int i = 0, j = 0;
+
+    while (i < n) {
+        if (j < n && (cnt[s[j] - 'a']== 0)) {
+
+            cnt[s[j] - 'a']++;
+
+            ans += (j - i + 1);
+            j++;
+        }
+
+        else {
+            cnt[s[i] - 'a']--;
+            i++;
+        }
+    }
+    return ans;
+}
+int main(){
+    string s;
+    cin>>s;
+    cout<<fun(s);
+}
